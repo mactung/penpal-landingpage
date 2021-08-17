60a77560b9288388793ae767137686faf114f6a2
MacTung <mactrantung@gmail.com>
2021-08-17 09:00:56 +0700

    update button register

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                                     <h1>Chúng tôi là <span class="color">CLB Penpal English</span></h1>
                                     <p>Mô hình học tập Tiếng Anh giao tiếp trực thuộc trường học Anh Quốc EV Academy. Tạo nền tảng cho con từng bước trở thành công dân toàn cầu với các CLB rèn luyện giao tiếp với người Anh (6 - 15 tuổi).</p>
 
-                                    <!-- <a class="btn read-more" href="#">Get started Now</a> -->
+                                    <a class="btn read-more" href="https://docs.google.com/forms/d/e/1FAIpQLScnTmuVuUTrCIWQa8GhN46sp3sPogbKXqt8oO7JeIugRgbEpg/viewform?usp=sf_link">Đăng kí ngay</a>
 
                                 </div>
                             </div>
@@ -154,7 +154,7 @@
                                         Tiết kiệm thời gian: Toàn bộ thời gian của giáo viên là dành cho bạn, không chia sẻ với bất cứ ai
                                     </p>
 
-                                    <!-- <a class="btn read-more" href="#">Get started Now</a> -->
+                                    <a class="btn read-more" href="https://docs.google.com/forms/d/e/1FAIpQLScnTmuVuUTrCIWQa8GhN46sp3sPogbKXqt8oO7JeIugRgbEpg/viewform?usp=sf_link">Đăng kí ngay</a>
 
                                 </div>
                             </div>
@@ -171,68 +171,53 @@
                         <div class="wpb_wrapper">
                             <div id="testimonial_block_61192d337432c" class="cvca-testimonial-shortcode cvca-testimonial relative home4-testimonial  vc_custom_1490554731056 style-1 carousel-testimonial">
                                 <h2 class="title-block">Đội ngũ giáo viên</h2>
-                                <div class="cvca-wrapper-testimonial-block cvca-testimonial-carousel" data-config=' {&quot;item&quot;:&quot;2&quot;,&quot;pagination&quot;:&quot;true&quot;,&quot;navigation&quot;:&quot;false&quot;} '>
-                                    <article id="testimonial-2796" class="cvca-testimonial-item col-xs-12 col-sm-6 post-2796 testimonial type-testimonial status-publish hentry testimonial_category-testimonial-02">
-                                        <div class="cvca-testimonial-content">
-                                            Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include storytelling, discussion, teaching, training, and directed research. Lorem ipsum dolor sit amet, consectetur adipiscing elit.... </div>
-                                        <div class="cvca-wrap-author">
-                                            <div class="media">
-                                                <div class="cvca-wrap-avatar pull-left">
-                                                    <img src="wp-content/uploads/2009/05/tes-3.jpg" alt="Testimonial 6" class="avatar-circus" />
-                                                </div>
-                                                <div class="cvca-wrap-author-info media-body">
-                                                    <h4 class="cvca-testimonial-author">Jennifer Lopez </h4>
-                                                    <div class="cvca-testimonial-des">Designer </div>
-                                                </div>
+                                <div class="cvca-wrapper-testimonial-block cvca-testimonial-carousel" data-config=' {"item":"2","pagination":"true","navigation":"false"} '>
+                                    <article id=" testimonial-2796" class="col-xs-12 col-sm-6 ">
+                                        <div class="media">
+                                            <div class="cvca-wrap-avatar">
+                                                <img src="wp-content/uploads/2009/05/tes-3.jpg" alt="Testimonial 6" class="avatar-circus" />
+                                            </div>
+                                            <div class="cvca-wrap-author-info media-body">
+                                                <h4 class="cvca-testimonial-author">Jennifer Lopez </h4>
+                                                <div class="cvca-testimonial-des">Designer </div>
                                             </div>
                                         </div>
                                     </article>
-                                    <article id="testimonial-2794" class="cvca-testimonial-item col-xs-12 col-sm-6 post-2794 testimonial type-testimonial status-publish hentry testimonial_category-testimonial-02">
-                                        <div class="cvca-testimonial-content">
-                                            Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include storytelling, discussion, teaching, training, and directed research.Lorem ipsum dolor sit amet, consectetur adipiscing elit.... </div>
-                                        <div class="cvca-wrap-author">
-                                            <div class="media">
-                                                <div class="cvca-wrap-avatar pull-left">
-                                                    <img src="wp-content/uploads/2009/05/tes-2.jpg" alt="Testimonial 5" class="avatar-circus" />
-                                                </div>
-                                                <div class="cvca-wrap-author-info media-body">
-                                                    <h4 class="cvca-testimonial-author">Jonh </h4>
-                                                    <div class="cvca-testimonial-des">Teacher </div>
-                                                </div>
+                                    <article id="testimonial-2796" class="col-xs-12 col-sm-6 ">
+                                        <div class="media">
+                                            <div class="cvca-wrap-avatar">
+                                                <img src="wp-content/uploads/2009/05/tes-3.jpg" alt="Testimonial 6" class="avatar-circus" />
+                                            </div>
+                                            <div class="cvca-wrap-author-info media-body">
+                                                <h4 class="cvca-testimonial-author">Jennifer Lopez </h4>
+                                                <div class="cvca-testimonial-des">Designer </div>
                                             </div>
                                         </div>
                                     </article>
-                                    <article id="testimonial-2794" class="cvca-testimonial-item col-xs-12 col-sm-6 post-2794 testimonial type-testimonial status-publish hentry testimonial_category-testimonial-02">
-                                        <div class="cvca-testimonial-content">
-                                            Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include storytelling, discussion, teaching, training, and directed research.Lorem ipsum dolor sit amet, consectetur adipiscing elit.... </div>
-                                        <div class="cvca-wrap-author">
-                                            <div class="media">
-                                                <div class="cvca-wrap-avatar pull-left">
-                                                    <img src="wp-content/uploads/2009/05/tes-2.jpg" alt="Testimonial 5" class="avatar-circus" />
-                                                </div>
-                                                <div class="cvca-wrap-author-info media-body">
-                                                    <h4 class="cvca-testimonial-author">Jonh </h4>
-                                                    <div class="cvca-testimonial-des">Teacher </div>
-                                                </div>
+                                    <article id="testimonial-2796" class="col-xs-12 col-sm-6 ">
+                                        <div class="media">
+                                            <div class="cvca-wrap-avatar">
+                                                <img src="wp-content/uploads/2009/05/tes-3.jpg" alt="Testimonial 6" class="avatar-circus" />
+                                            </div>
+                                            <div class="cvca-wrap-author-info media-body">
+                                                <h4 class="cvca-testimonial-author">Jennifer Lopez </h4>
+                                                <div class="cvca-testimonial-des">Designer </div>
                                             </div>
                                         </div>
                                     </article>
-                                    <article id="testimonial-2792" class="cvca-testimonial-item col-xs-12 col-sm-6 post-2792 testimonial type-testimonial status-publish hentry testimonial_category-testimonial-02">
-                                        <div class="cvca-testimonial-content">
-                                            Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include storytelling, discussion, teaching, training, and directed research.Lorem ipsum dolor sit amet, consectetur adipiscing elit.... </div>
-                                        <div class="cvca-wrap-author">
-                                            <div class="media">
-                                                <div class="cvca-wrap-avatar pull-left">
-                                                    <img src="wp-content/uploads/2009/05/tes-1.png" alt="Testimonial 4" class="avatar-circus" />
-                                                </div>
-                                                <div class="cvca-wrap-author-info media-body">
-                                                    <h4 class="cvca-testimonial-author">Julia miller </h4>
-                                                    <div class="cvca-testimonial-des">Studient </div>
-                                                </div>
+                                    <article id="testimonial-2796" class="col-xs-12 col-sm-6 ">
+                                        <div class="media">
+                                            <div class="cvca-wrap-avatar">
+                                                <img src="wp-content/uploads/2009/05/tes-3.jpg" alt="Testimonial 6" class="avatar-circus" />
+                                            </div>
+                                            <div class="cvca-wrap-author-info media-body">
+                                                <h4 class="cvca-testimonial-author">Jennifer Lopez </h4>
+                                                <div class="cvca-testimonial-des">Designer </div>
                                             </div>
                                         </div>
                                     </article>
                                 </div>
+
                             </div>
                         </div>
                     </div>
@@ -260,7 +245,7 @@
                                     </p>
 
 
-                                    <!-- <a class="btn read-more" href="#">Get started Now</a> -->
+                                    <a class="btn read-more" href="https://docs.google.com/forms/d/e/1FAIpQLScnTmuVuUTrCIWQa8GhN46sp3sPogbKXqt8oO7JeIugRgbEpg/viewform?usp=sf_link">Đăng kí ngay</a>
 
                                 </div>
                             </div>
